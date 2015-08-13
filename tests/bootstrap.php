cff620deb8a3fa9dddf0adcaccff85e262d77dfa
dcb9 <jt1dcb@163.com>
2015-08-13 11:52:56 +0800

    fix tests bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,14 @@
 <?php
 
-    $base_dir = dirname( dirname ( dirname(__DIR__) ) );
+if (file_exists(dirname(__DIR__) . '/vendor/autoload.php')) {
+    $loaderFile = dirname(__DIR__) . '/vendor/autoload.php';
+} elseif (file_exists('../../../autoload.php')) {
+    $loaderFile = '../../../autoload.php';
+} elseif (file_exists(dirname(dirname(dirname(__DIR__))) . '/autoload.php')) {
+    $loaderFile = dirname(dirname(dirname(__DIR__))) . '/autoload.php';
+} else {
+    echo 'autoload.php file not found';
+    exit(1);
+}
 
-    $loader = require $base_dir . '/autoload.php';
-    $loader->add('AppName', __DIR__.'/../src/');
-
-    
+require_once($loaderFile);
